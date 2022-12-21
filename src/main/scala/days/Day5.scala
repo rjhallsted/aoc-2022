0101529b0444117ce34c69c4bc247b52c9708305
rjhallsted <rjhallsted@gmail.com>
2022-12-21 11:38:57 -0800

    Day 5

--- src/main/scala/days/Day5.scala
+++ src/main/scala/days/Day5.scala
@@ -5,22 +5,33 @@ object Day5 extends Day {
     if (item(1) == ' ') None else Some(item(1))
   }
 
-  def buildStacks(stackString: String): List[List[Char]] = {
+  def buildStacks(stackString: String): IndexedSeq[List[Char]] = {
     val colCount =
       (stackString.indexOf("\n") / 4) + 1
 
     val stackItems = stackString.grouped(4).map(parseStackItem).toIndexedSeq
 
-    println(s"cols: $colCount")
-    println(s"items: ${stackItems.size}")
-
     val stacks = (0 until colCount)
       .map(col =>
         (0 until stackItems.size).filter(_ % colCount == col).map(stackItems(_)).toList
       )
-      .toList
+      .toIndexedSeq
 
     stacks.map(_.flatMap(_.filter(_.isLetter).toList))
+  }
+
+  final case class Instruction(
+    count: Int,
+    from: Int,
+    to: Int
+  )
+
+  def executeInstruction(stack: IndexedSeq[List[Char]], ins: Instruction): IndexedSeq[List[Char]] = {
+    val moving = stack(ins.from).take(ins.count)
+    val removed = stack.updated(ins.from, stack(ins.from).drop(ins.count))
+    val added = removed.updated(ins.to, moving ++ removed(ins.to))
+
+    added
   }
 
   def main(input: String): Unit = {
@@ -30,6 +41,15 @@ object Day5 extends Day {
 
     val stacks = buildStacks(initialStacksString)
 
-    ???
+    val instructionPattern = """move (\d+) from (\d+) to (\d+)""".r
+    val instructions: List[Instruction] = instructionsStrings.map(s => {
+        val instructionPattern(count, from, to) = s
+        Instruction(count.toInt, from.toInt - 1, to.toInt - 1)
+    })
+
+    val finished = instructions.foldLeft(stacks)(executeInstruction)
+    val res = finished.flatMap(_.take(1)).mkString
+
+    println(res)
   }
 }
